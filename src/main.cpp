0e9ad388bd9f8ebd4fcb5a2afcdc96df171f4d45
Farès Chati <faresticha@gmail.com>
2019-05-29 10:30:41 +0200

    Programmation du capteur de sol

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,24 @@
 #include <mbed.h>
+#include <sht15.hpp>
 
-int main() {
+SHTx::SHT15 soilSensor (I2C_SDA, I2C_SCL);
 
-  // put your setup code here, to run once:
+int main ()
+{
+	soilSensor.setOTPReload (false);
+	soilSensor.setResolution (true);
+	soilSensor.setScale (false);
 
-  while(1) {
-    // put your main code here, to run repeatedly:
-  }
+	while (1)
+	{
+		soilSensor.update();
+
+		float temp = (soilSensor.getTemperature());	// Temperature - Sol
+		float humi = (soilSensor.getHumidity());	// Humidity - Sol
+
+		printf ("Température : %.2f°C\n", temp);
+		printf ("Humidité : %.0f%%\n\n\n", humi);
+
+		wait (2);
+	}
 }
